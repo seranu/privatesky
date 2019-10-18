158fab3c2cad02fbfae8d81982b55fdb9deb82e4
StefanCosminR <stefancosmin.romanescu@gmail.com>
2019-10-18 11:00:31 +0300

    Fixed moving swarm execution from one agent to another

--- psknode/core/sandboxes/AgentsManager.js
+++ psknode/core/sandboxes/AgentsManager.js
@@ -1,13 +1,14 @@
 const {Agent} = require('./Agent');
+const OwM = require('swarmutils').OwM;
 
 
 function AgentsManager({constitutions, workDir}) {
-    if(!this instanceof AgentsManager) {
+    if (!this instanceof AgentsManager) {
         throw new TypeError('Calling this constructor without new is forbidden');
     }
 
     const generalAgent = new Agent(constitutions, workDir, 'threads');
-    $$.PSK_PubSub.subscribe($$.CONSTANTS.SWARM_FOR_EXECUTION, executeSwarm);
+    $$.PSK_PubSub.subscribe($$.CONSTANTS.SWARM_FOR_EXECUTION, executeSwarm, filterSwarmsExecutionRequests);
 
     function isOwnAgent(agent) {
         return true;
@@ -16,21 +17,38 @@ function AgentsManager({constitutions, workDir}) {
     function executeSwarm(swarm) {
         $$.log("Executing in sandbox towards: ", swarm.meta.target);
 
-        if(isOwnAgent(swarm.meta.target)) {
-            generalAgent.executeSwarm(swarm, (err, newSwarm) => {
-                if(err) {
-                    $$.error('error executing in worker pool', err);
-                    // do something
-                    return;
-                }
+        generalAgent.executeSwarm(swarm, (err, newSwarm) => {
+            if (err) {
+                $$.error('error executing in worker pool', err);
+                // do something
+                return;
+            }
 
+            newSwarm = new OwM(newSwarm);
+
+            if (newSwarm.getMeta('command') === 'executeSwarmPhase') {
+                $$.PSK_PubSub.publish($$.CONSTANTS.SWARM_FOR_EXECUTION, newSwarm);
+            } else {
                 $$.PSK_PubSub.publish($$.CONSTANTS.SWARM_RETURN, newSwarm);
-            });
-        } else {
-            $$.log('Is not own agent');
-            $$.PSK_PubSub.publish($$.CONSTANTS.SWARM_RETURN, swarm);
+            }
+        });
+
+    }
+
+    function filterSwarmsExecutionRequests(swarm) {
+        swarm = new OwM(swarm);
+
+        if (!isOwnAgent(swarm.getMeta('target'))) {
+            $$.error(`Received swarm for an agent ${swarm.getMeta('target')} that does not exist in domain ${process.env.PRIVATESKY_DOMAIN_NAME}`);
+            return false;
         }
 
+        if (swarm.getMeta('command') !== 'executeSwarmPhase') {
+            $$.error(`Received swarm with wrong command ${swarm.getMeta('command')}`);
+            return false;
+        }
+
+        return true;
     }
 
 }
