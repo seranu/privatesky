8181f64c64b7aec284af814fbecd57606fc1a889
StefanCosminR <stefancosmin.romanescu@gmail.com>
2019-11-12 13:22:58 +0200

    Added /psknode/bin/cmds to npm run bash environment

--- bin/bashEnv.sh
+++ bin/bashEnv.sh
@@ -14,6 +14,7 @@ source ~/.bashrc
 
 current_bin_path="${current_path}/bin"
 psknode_bin_path="${current_path}/psknode/bin"
+psknode_bin_path="${current_path}/psknode/bin/cmds"
 
 export PATH="${current_bin_path}:${psknode_bin_path}:$PATH"
 export PS1="[PSK-BASH \W]$ "
